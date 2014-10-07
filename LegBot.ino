11565a14b8c2654ee889b442a54e24ed8b727497
Ken Sharp <sharpk60@yahoo.com>
2014-10-07 13:42:01 -0500

    Initial version of LegBot code

---
+++ LegBot.ino
@@ -0,0 +1,78 @@
+// LegBot obstacle avoiding Robot
+// The LegBot was modeled after the inchworm bot here, with an Arduino and Ping sensor added:
+// http://www.instructables.com/id/Simple-Bots-Inchworm/
+//  Ken Sharp, Oct 2014
+
+#include <Servo.h>
+
+// Pin Mapping
+const int motorPin = 10;
+const int pingPin = 7;
+const int switchPin = 2;
+
+// There's a good chance this will have to be changed to "LOW"
+// based on how the switch is wired to the Arduino.
+const int switchPolarity = HIGH;
+
+// Set the speed that the servo will move.
+// Range is 0-90 with higher numbers meaning the servo will move faster.
+const int motorSpeed = 15;
+
+// Threshold for obstacles (in cm)
+const int dangerThreshold = 10;
+
+Servo motor;
+int currentMotorSpeed;
+
+void setup() {
+	// Init main servo
+	motor.attach(motorPin);
+	
+	// Init switch input and enable pullup resistor
+	pinMode(switchPin, INPUT_PULLUP);
+	
+	delay(5000);
+}
+
+void loop() {
+	int fwdDistance = ping();
+
+	if (fwdDistance < dangerThreshold) {
+		// stop the motor, there's an obstacle in our way
+		motor.write(90);
+	}
+	else if (digitalRead(switchPin) == switchPolarity) {
+		// if the switch has not flipped yet, keep going forward
+		if (currentMotorSpeed != 90 + motorSpeed) {
+			currentMotorSpeed = 90 + motorSpeed;
+			motor.write(currentMotorSpeed);
+		}
+	}
+	else {
+		// after the switch flips move backward
+		if (currentMotorSpeed != 90 - motorSpeed) {
+			currentMotorSpeed = 90 - motorSpeed;
+			motor.write(currentMotorSpeed);
+		}
+	}
+}
+
+long ping()
+{
+	int duration;
+	
+	// Send out PING))) signal pulse
+	pinMode(pingPin, OUTPUT);
+	digitalWrite(pingPin, LOW);
+	delayMicroseconds(2);
+	digitalWrite(pingPin, HIGH);
+	delayMicroseconds(5);
+	digitalWrite(pingPin, LOW);
+
+	//Get duration it takes to receive echo
+	pinMode(pingPin, INPUT);
+	duration = pulseIn(pingPin, HIGH);
+
+	//Convert duration into distance
+	return duration / 29 / 2;
+}
